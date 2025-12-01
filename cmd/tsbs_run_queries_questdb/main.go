bd1658425ce8ef6fcccd459c647174fc84586610
Vlad Ilyushchenko <bluestreak@gmail.com>
2025-12-01 21:32:46 +0000

    use pgx for questdb

--- cmd/tsbs_run_queries_questdb/main.go
+++ cmd/tsbs_run_queries_questdb/main.go
@@ -1,14 +1,18 @@
-// tsbs_run_queries_influx speed tests InfluxDB using requests from stdin.
+// tsbs_run_queries_questdb speed tests QuestDB using requests from stdin or file.
 //
-// It reads encoded Query objects from stdin, and makes concurrent requests
-// to the provided HTTP endpoint. This program has no knowledge of the
-// internals of the endpoint.
+// It reads encoded Query objects from stdin or file, and makes concurrent requests
+// to the provided endpoint. Supports both HTTP/JSON and PostgreSQL wire protocol (pgx v5).
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/blagojts/viper"
+	"github.com/jackc/pgx/v5"
 	"github.com/questdb/tsbs/internal/utils"
 	"github.com/questdb/tsbs/pkg/query"
 	"github.com/spf13/pflag"
@@ -19,6 +23,13 @@ var (
 	restURL  string
 	username string
 	password string
+	// PostgreSQL mode options
+	usePgx   bool
+	pgHost   string
+	pgPort   string
+	pgUser   string
+	pgPass   string
+	pgDBName string
 )
 
 // Global vars:
@@ -31,9 +42,18 @@ func init() {
 	var config query.BenchmarkRunnerConfig
 	config.AddToFlagSet(pflag.CommandLine)
 
-	pflag.String("url", "http://localhost:9000/", "Server URL. In case of HTTPS, the client will not validate the certificate, i.e. it trusts any server")
-	pflag.String("username", "", "Basic auth username")
-	pflag.String("password", "", "Basic auth password")
+	// HTTP options
+	pflag.String("url", "http://localhost:9000/", "Server URL for HTTP mode")
+	pflag.String("username", "", "Basic auth username (HTTP mode)")
+	pflag.String("password", "", "Basic auth password (HTTP mode)")
+
+	// PostgreSQL/pgx options
+	pflag.Bool("use-pgx", false, "Use PostgreSQL wire protocol (pgx v5) instead of HTTP")
+	pflag.String("pg-host", "localhost", "PostgreSQL host")
+	pflag.String("pg-port", "8812", "PostgreSQL port")
+	pflag.String("pg-user", "admin", "PostgreSQL user")
+	pflag.String("pg-pass", "quest", "PostgreSQL password")
+	pflag.String("pg-db", "qdb", "PostgreSQL database name")
 
 	pflag.Parse()
 
@@ -51,6 +71,13 @@ func init() {
 	username = viper.GetString("username")
 	password = viper.GetString("password")
 
+	usePgx = viper.GetBool("use-pgx")
+	pgHost = viper.GetString("pg-host")
+	pgPort = viper.GetString("pg-port")
+	pgUser = viper.GetString("pg-user")
+	pgPass = viper.GetString("pg-pass")
+	pgDBName = viper.GetString("pg-db")
+
 	runner = query.NewBenchmarkRunner(config)
 }
 
@@ -59,29 +86,96 @@ func main() {
 }
 
 type processor struct {
-	w    *HTTPClient
-	opts *HTTPClientDoOptions
+	// HTTP mode
+	httpClient *HTTPClient
+	httpOpts   *HTTPClientDoOptions
+	// pgx v5 mode - native connection (not database/sql)
+	conn *pgx.Conn
+	ctx  context.Context
 }
 
 func newProcessor() query.Processor { return &processor{} }
 
 func (p *processor) Init(workerNumber int) {
-	p.opts = &HTTPClientDoOptions{
-		Username:             username,
-		Password:             password,
-		Debug:                runner.DebugLevel(),
-		PrettyPrintResponses: runner.DoPrintResponses(),
+	if usePgx {
+		connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			pgHost, pgPort, pgUser, pgPass, pgDBName)
+		p.ctx = context.Background()
+		conn, err := pgx.Connect(p.ctx, connStr)
+		if err != nil {
+			panic(fmt.Sprintf("Unable to connect to QuestDB via pgx v5: %v", err))
+		}
+		p.conn = conn
+	} else {
+		p.httpOpts = &HTTPClientDoOptions{
+			Username:             username,
+			Password:             password,
+			Debug:                runner.DebugLevel(),
+			PrettyPrintResponses: runner.DoPrintResponses(),
+		}
+		p.httpClient = NewHTTPClient(restURL)
 	}
-	p.w = NewHTTPClient(restURL)
 }
 
 func (p *processor) ProcessQuery(q query.Query, _ bool) ([]*query.Stat, error) {
 	hq := q.(*query.HTTP)
-	lag, err := p.w.Do(hq, p.opts)
+
+	var lag float64
+	var err error
+
+	if usePgx {
+		lag, err = p.processQueryPgx(hq)
+	} else {
+		lag, err = p.httpClient.Do(hq, p.httpOpts)
+	}
+
 	if err != nil {
 		return nil, err
 	}
 	stat := query.GetStat()
 	stat.Init(q.HumanLabelName(), lag)
 	return []*query.Stat{stat}, nil
+}
+
+// processQueryPgx extracts SQL from HTTP query and runs it via native pgx v5
+func (p *processor) processQueryPgx(hq *query.HTTP) (float64, error) {
+	// Extract SQL from HTTP path: /exec?count=false&query=SELECT...
+	pathStr := string(hq.Path)
+
+	// Parse URL to extract query parameter
+	// The path looks like: /exec?count=false&query=SELECT...
+	if idx := strings.Index(pathStr, "?"); idx != -1 {
+		queryStr := pathStr[idx+1:]
+		values, err := url.ParseQuery(queryStr)
+		if err != nil {
+			return 0, fmt.Errorf("failed to parse query params: %v", err)
+		}
+
+		sqlQuery := values.Get("query")
+		if sqlQuery == "" {
+			return 0, fmt.Errorf("no SQL query found in path: %s", pathStr)
+		}
+
+		start := time.Now()
+
+		// Use native pgx Query (not database/sql) for better performance
+		rows, err := p.conn.Query(p.ctx, sqlQuery)
+		if err != nil {
+			return 0, fmt.Errorf("query failed: %v", err)
+		}
+
+		// Fetch all rows - same approach as TimescaleDB benchmark
+		for rows.Next() {
+		}
+		rows.Close()
+
+		if err := rows.Err(); err != nil {
+			return 0, fmt.Errorf("row iteration error: %v", err)
+		}
+
+		lag := float64(time.Since(start).Nanoseconds()) / 1e6 // milliseconds
+		return lag, nil
+	}
+
+	return 0, fmt.Errorf("invalid path format: %s", pathStr)
 }
--- go.mod
+++ go.mod
@@ -1,37 +1,82 @@
 module github.com/questdb/tsbs
 
-go 1.14
+go 1.23.0
 
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.0.0
 	github.com/SiriDB/go-siridb-connector v0.0.0-20190110105621-86b34c44c921
-	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/andreyvit/diff v0.0.0-20170406064948-c7f18ee00883
 	github.com/aws/aws-sdk-go v1.35.13
 	github.com/blagojts/viper v1.6.3-0.20200313094124-068f44cf5e69
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
-	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/gocql/gocql v0.0.0-20190810123941-df4b9cc33030
 	github.com/golang/protobuf v1.4.2
 	github.com/golang/snappy v0.0.1
 	github.com/google/flatbuffers v1.11.0
-	github.com/google/go-cmp v0.5.2
+	github.com/google/go-cmp v0.6.0
 	github.com/jackc/pgx/v4 v4.8.0
+	github.com/jackc/pgx/v5 v5.7.6
 	github.com/jmoiron/sqlx v1.2.1-0.20190826204134-d7d95172beb5
 	github.com/kshvakov/clickhouse v1.3.11
 	github.com/lib/pq v1.3.0
 	github.com/pkg/errors v0.9.1
-	github.com/pkg/profile v1.2.1
 	github.com/prometheus/common v0.13.0
 	github.com/shirou/gopsutil v3.21.3+incompatible
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5
 	github.com/timescale/promscale v0.0.0-20201006153045-6a66a36f5c84
-	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/transceptor-technology/go-qpack v0.0.0-20190116123619-49a14b216a45
 	github.com/valyala/fasthttp v1.15.1
 	go.uber.org/atomic v1.6.0
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73
+	golang.org/x/net v0.25.0
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
 	gopkg.in/yaml.v2 v2.3.0
+)
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.14.8 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.6.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgtype v1.4.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.10.10 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.2.2 // indirect
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/sergi/go-diff v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d // indirect
+	google.golang.org/grpc v1.32.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/ini.v1 v1.51.0 // indirect
 )
